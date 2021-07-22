dd4374f3980a841031095f328e318610bf9ec311
Lucas(주영준) <jyj@gabia.com>
2021-07-22 14:29:22 +0900

    feat: webhook consumer logic 변경

--- src/main/java/gabia/logConsumer/dto/WebhookDTO.java
+++ src/main/java/gabia/logConsumer/dto/WebhookDTO.java
@@ -1,11 +1,6 @@
 package gabia.logConsumer.dto;
 
-import gabia.logConsumer.entity.Enum.NoticeType;
-import gabia.logConsumer.util.ValidUUID;
-import java.sql.Timestamp;
-import java.util.UUID;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import gabia.logConsumer.entity.Enum.WebhookEndpoint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,14 +12,17 @@ public class WebhookDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Request {
+
         String url;
         String text;
+        WebhookEndpoint endpoint;
     }
 
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SendText {
+
         String text;
     }
 
